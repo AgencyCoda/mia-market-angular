16b6cdcc2c673294b07011aaa0e812b53bead82f
Matias Camiletti <matias.camiletti@gmail.com>
2021-12-09 17:37:36 -0300

    Add entities and services.

--- projects/agencycoda/mia-market-core/src/lib/mia-market-core.module.ts
+++ projects/agencycoda/mia-market-core/src/lib/mia-market-core.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
-import { MiaMarketCoreComponent } from './mia-market-core.component';
-
 
 
 @NgModule({
   declarations: [
-    MiaMarketCoreComponent
   ],
   imports: [
   ],
   exports: [
-    MiaMarketCoreComponent
   ]
 })
 export class MiaMarketCoreModule { }
--- projects/agencycoda/mia-market-core/src/public-api.ts
+++ projects/agencycoda/mia-market-core/src/public-api.ts
@@ -2,6 +2,25 @@
  * Public API Surface of mia-market-core
  */
 
-export * from './lib/mia-market-core.service';
-export * from './lib/mia-market-core.component';
+/**
+ * Entities
+ */
+ export * from './lib/entities/mia_product';
+ export * from './lib/entities/mia_product_child';
+ export * from './lib/entities/mia_product_stock';
+ export * from './lib/entities/mia_store';
+ export * from './lib/entities/mia_store_permission';
+
+/**
+ * Services
+ */
+ export * from './lib/services/mia_product.service';
+ export * from './lib/services/mia_product_child.service';
+ export * from './lib/services/mia_product_stock.service';
+ export * from './lib/services/mia_store.service';
+ export * from './lib/services/mia_store_permission.service';
+
+/**
+ * Modules
+ */
 export * from './lib/mia-market-core.module';
